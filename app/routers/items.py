2183e213a6a712aa2fa8693661682537d78d7fca
Miguel Hernandez <mhernand@thoughtworks.com>
2020-09-17 10:07:02 -0500

    Solves linter warnings

--- app/routers/items.py
+++ app/routers/items.py
@@ -4,18 +4,22 @@ from pydantic import BaseModel
 
 router = APIRouter()
 
+
 class Item(BaseModel):
     name: str
     price: float
     is_offer: Optional[bool] = None
 
+
 @router.get("/", tags=["items"])
 def read_root():
     return {"msg": "Hello World"}
 
+
 @router.get("/items/{item_id}", tags=["items"])
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
 
 @router.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,12 +4,14 @@ from app.main import app
 
 client = AsyncClient()
 
+
 @pytest.mark.asyncio
 async def test_read_root():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
+
 
 @pytest.mark.asyncio
 async def test_read_item():
@@ -18,12 +20,14 @@ async def test_read_item():
     assert response.status_code == 200
     assert response.json() == {"item_id": 1, "q": None}
 
+
 @pytest.mark.asyncio
 async def test_update_item():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.put("/items/1", json={"name": "foobar", "price": 122.11})
     assert response.status_code == 200
     assert response.json() == {"item_name": "foobar", "item_id": 1}
+
 
 @pytest.mark.asyncio
 async def test_update_item_fail():
