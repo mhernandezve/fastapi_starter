203d300a21ba35e0ffe4edfd1a1465755f82f1c6
Miguel Hernandez <mh@andsoluciones.com>
2020-09-18 16:05:23 -0500

    Set endpoints as async

--- app/routers/items.py
+++ app/routers/items.py
@@ -12,15 +12,15 @@ class Item(BaseModel):
 
 
 @router.get("/", tags=["items"])
-def read_root():
+async def read_root():
     return {"msg": "Hello World"}
 
 
 @router.get("/items/{item_id}", tags=["items"])
-def read_item(item_id: int, q: Optional[str] = None):
+async def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 
 @router.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
+async def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
